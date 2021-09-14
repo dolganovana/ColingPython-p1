ce041316953df2375f080ade513c95f5e25fa68a
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-09-15 02:01:42 +0300

    Update B.py

--- HW1/B/B.py
+++ HW1/B/B.py
@@ -1,3 +1,7 @@
 def solution(n):
-    ### YOUR SOLUTION ###
+    print(n * "   _~_   ")
+    print(n * "  (o o)  ")
+    print(n * " /  V  \ ")
+    print(n * "/(  _  )\")
+    print(n * "  ^^ ^^  ")
     return
