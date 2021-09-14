20fb61e8f94bba1c84c40ff6a83ee9ea94d6be9f
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-09-15 02:27:45 +0300

    Update D.py

--- HW1/D/D.py
+++ HW1/D/D.py
@@ -1,3 +1,6 @@
 def solution(total):
-    ### YOUR CODE HERE ###
+    total2 = total % 1440
+    hours = total2 // 60
+    minutes = total2 % 60
+    print(str(hours) + ' ' + str(minutes))
     return
