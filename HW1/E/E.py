084128aa4f77125d3bbef593a1122772c8bd1aff
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-09-15 14:20:10 +0300

    Update E.py

--- HW1/E/E.py
+++ HW1/E/E.py
@@ -1,3 +1,11 @@
 def solution(x1, y1, x2, y2):
-    ### YOUR CODE ###
+    
+    check = False
+
+    if abs(x2 - x1) < 2:
+        if abs(y2 - y1) < 2:
+            check = True
+            
+    print(check)
+
     return
