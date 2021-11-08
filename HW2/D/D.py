176c3bf9b9b3018ebbcbfdcef2dba10ec7b6fbfc
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-11-09 00:09:00 +0300

    Update D.py

--- HW2/D/D.py
+++ HW2/D/D.py
@@ -1,3 +1,24 @@
 def solution(n,k):
-    ### YOUR CODE HERE ###
-    return
+    my_list = []
+    c = 1
+    count = 1
+    t = True
+    for i in range(1, n+1):
+        my_list.append(c)
+        c += 1
+
+    while t:
+        for i in my_list:
+            #print(i, count)
+            if count == k:
+                my_list.remove(i)
+                #print(i, count, my_list)
+                count = 1 
+            else:
+                count += 1
+
+        if len(my_list) == 1:
+            t = False
+    print(my_list)
+
+    return my_list[0]
