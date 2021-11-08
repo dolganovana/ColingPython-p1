57125428377b5b0109981f542db56b28eec8e52c
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-11-08 23:53:01 +0300

    Update B.py

--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,13 @@
 def solution(x):
-    ### YOUR CODE ###
+    mystring = x
+    newstring = ""
+    for i in range(0, len(mystring)):
+        if ((i == 0) or (i % 3 != 0)):
+            if (mystring[i] == "h") and ((i != mystring.find("h")) or (i != mystring.rfind("h"))):
+                newstring = newstring + "H"
+            else:
+                newstring = newstring + mystring[i]
+
+    newstring = newstring.replace("1", "one")
+    print(newstring)
     return
