f588b32309419cb14a1710bea5c827562d8bc06c
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-12-02 03:19:02 +0300

    Update G.py

--- HW2/G/G.py
+++ HW2/G/G.py
@@ -1,3 +1,11 @@
 def solution(a, b):
-    ### YOUR CODE ###
-    return
+    new = a.copy()
+    for i in b:
+        if i in a:
+            continue
+        else:
+            new.append(i)
+
+    new.sort()
+
+    return new
