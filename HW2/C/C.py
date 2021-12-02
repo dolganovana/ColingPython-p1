1a8668a7c5a578d08699a25257f0017111f4c4f8
Dolganova Nina <90354196+dolganovana@users.noreply.github.com>
2021-12-02 03:03:58 +0300

    Update C.py

--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,13 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+    newlist = []
+    count = 1
+    f = True
+    while f:
+        if count % 2 == 1:
+            for i in arr[count]:
+                newlist.append(i)
+
+        f = False
+
+
+    return newlist
